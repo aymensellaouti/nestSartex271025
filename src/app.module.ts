9d53e9cf150fec971ed39cb7ee60f2551610bc7f
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-27 11:52:18 +0100

    module exemples

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirstModule } from './first/first.module';
+import { SecondModule } from './second/second.module';
 
 @Module({
-  imports: [],
+  // Eli nest7a9ou 
+  imports: [FirstModule, SecondModule],
+  // 
   controllers: [AppController],
+  // Taw nchoufou
   providers: [AppService],
 })
 export class AppModule {}
