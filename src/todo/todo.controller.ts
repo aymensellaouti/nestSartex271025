5fc98c1ac3ae9f8f087f4bf6e22f7138d09433ba
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-28 11:10:14 +0100

    Go To Service

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,47 +1,31 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { TodoModel } from './todo.model';
-import { v4 as uuidv4 } from 'uuid';
+
 import { TodoControllerInterface } from './todo-controller.interface';
 import { AddTodoDto } from './dto/add-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
-import { LoggerService } from '../common/logger.service';
+import { TodoService } from './todo.service';
 @Controller('todo')
 export class TodoController implements TodoControllerInterface {
-    constructor(private logger: LoggerService) {}
-    private todos = [
-        //new TodoModel(uuidv4(), "NestJS", "faire l'exercice")
-    ];
-
-    
+    constructor(private todoService: TodoService) {}
     @Get('')
     getTodos(): TodoModel[] {
-        this.logger.log('Getting Todos');
-        return this.todos;
+        return this.todoService.getTodos();
     }
-
     @Get(':id')
     getTestTodos(@Param('id') id: string): TodoModel {
-        return this.findTodoById(id);
+        return this.todoService.getTestTodos(id);
     } 
-    
     @Delete(':id')
     deleteTodo(@Param('id') id: string): {count: number} {
-        const index = this.todos.findIndex((todo) => todo.id == id);
-        if (index == -1) return {count: 0};
-        this.todos.splice(index, 1)
-        return {count: 1};
+        return this.todoService.deleteTodo(id);    
     }    
-
     @Post()
     addTodo(
         @Body() addTodoDto: AddTodoDto
     ): TodoModel {
-        const {name, description} = addTodoDto;
-        const newTod =  new TodoModel(uuidv4(), name, description)
-        this.todos.push(newTod);
-        return newTod;
+        return this.todoService.addTodo(addTodoDto);
     }
-
     @Patch(':id')
     updateTodo(
         // Jibli el Body el kol
@@ -49,20 +33,6 @@ export class TodoController implements TodoControllerInterface {
         // Jibli el paramétre eli esmou id
         @Param('id') id: string
     ): TodoModel {
-        
-        const {name, description, status} = updateTodoDto;
-        const todoTodoUpdate = this.findTodoById(id);
-        console.log({todoTodoUpdate});
-        todoTodoUpdate.name = name ?? todoTodoUpdate.name;
-        todoTodoUpdate.description = description ?? todoTodoUpdate.description;
-        todoTodoUpdate.status = status ?? todoTodoUpdate.status;
-
-        return todoTodoUpdate;
-    }
-
-    findTodoById(id: string): TodoModel {
-        const todo = this.todos.find((todo) => todo.id == id);
-        if (!todo) throw new NotFoundException('Todo innexistant');
-        return todo;
+       return this.todoService.updateTodo(id,updateTodoDto);
     }
 }
---
+++ src/todo/todo.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, LoggerService, NotFoundException } from '@nestjs/common';
+import { AddTodoDto } from './dto/add-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodoModel } from './todo.model';
+import { v4 as uuidv4 } from 'uuid';
+@Injectable()
+export class TodoService {
+    constructor(private logger: LoggerService) { }
+    private todos = [
+        //new TodoModel(uuidv4(), "NestJS", "faire l'exercice")
+    ];
+
+    getTodos(): TodoModel[] {
+        this.logger.log('Getting Todos');
+        return this.todos;
+    }
+
+    getTestTodos(id: string): TodoModel {
+        return this.findTodoById(id);
+    }
+
+    deleteTodo(id: string): { count: number } {
+        const index = this.todos.findIndex((todo) => todo.id == id);
+        if (index == -1) return { count: 0 };
+        this.todos.splice(index, 1)
+        return { count: 1 };
+    }
+
+    addTodo(
+        addTodoDto: AddTodoDto
+    ): TodoModel {
+        const { name, description } = addTodoDto;
+        const newTod = new TodoModel(uuidv4(), name, description)
+        this.todos.push(newTod);
+        return newTod;
+    }
+
+    updateTodo(
+        id: string,
+        updateTodoDto: UpdateTodoDto,
+    ): TodoModel {
+
+        const { name, description, status } = updateTodoDto;
+        const todoTodoUpdate = this.findTodoById(id);
+        console.log({ todoTodoUpdate });
+        todoTodoUpdate.name = name ?? todoTodoUpdate.name;
+        todoTodoUpdate.description = description ?? todoTodoUpdate.description;
+        todoTodoUpdate.status = status ?? todoTodoUpdate.status;
+
+        return todoTodoUpdate;
+    }
+
+    findTodoById(id: string): TodoModel {
+        const todo = this.todos.find((todo) => todo.id == id);
+        if (!todo) throw new NotFoundException('Todo innexistant');
+        return todo;
+    }
+}
