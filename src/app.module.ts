03cb428c7b5d8068f489c2c6dd1a5dcfb3dd5f4f
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-29 12:02:52 +0100

    mise en place de la bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,24 @@ import { TodoModule } from './todo/todo.module';
 import { CommonModule } from './common/common.module';
 import { FirstMiddleware } from './common/middleware/first/first.middleware';
 import { AuthMiddleware } from './common/middleware/auth/auth.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   // Eli nest7a9ou 
-  imports: [FirstModule, SecondModule, TodoModule, CommonModule],
+  imports: [
+    FirstModule, SecondModule, TodoModule,
+    CommonModule,
+    TypeOrmModule.forRoot({ 
+      type: 'mysql', 
+      host: 'localhost', port: 3306, 
+      username: 'root', 
+      password: '', 
+      database: 'sartex', 
+      entities: [], 
+      synchronize: true, 
+      logging: true
+    })
+  ],
   // 
   controllers: [AppController],
   // Taw nchoufou
@@ -20,11 +34,11 @@ import { AuthMiddleware } from './common/middleware/auth/auth.middleware';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(AuthMiddleware)
-    .forRoutes(
-      //TodoController
-        {path: 'todo*', method: RequestMethod.PATCH},              
-        {path: 'todo*', method: RequestMethod.POST}, 
-        {path: 'todo*', method: RequestMethod.DELETE}, 
-    );
+      .forRoutes(
+        //TodoController
+        { path: 'todo*', method: RequestMethod.PATCH },
+        { path: 'todo*', method: RequestMethod.POST },
+        { path: 'todo*', method: RequestMethod.DELETE },
+      );
   }
 }
