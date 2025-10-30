dbfbd11c1fa2056679294b97d8b2414731d558ef
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-30 13:30:26 +0100

    cv dto and entity

--- src/cv/dto/create-cv.dto.ts
+++ src/cv/dto/create-cv.dto.ts
@@ -1,1 +1,15 @@
-export class CreateCvDto {}
+import { Type } from "class-transformer";
+import { IsString, IsNumber, IsOptional } from "class-validator";
+
+export class CreateCvDto {
+  @IsString()
+  name: string;
+  @IsNumber()
+  @Type(() => Number)
+  age: number;
+  @IsString()
+  job: string;
+  @IsString()
+  @IsOptional()
+  path: string;
+}
--- src/cv/entities/cv.entity.ts
+++ src/cv/entities/cv.entity.ts
@@ -1,1 +1,16 @@
-export class Cv {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { TimeStampEntity } from "../../common/db/timestamp.entity";
+
+@Entity('cv')
+export class Cv extends TimeStampEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    @Column()
+    name: string;
+    @Column()
+    age: number;
+    @Column()
+    job: string;
+    @Column()
+    path: string;
+}
