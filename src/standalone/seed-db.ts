f7322757dd5807882ca6b32c79b4dee747a96199
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-30 14:18:49 +0100

    mise en place falso

--- src/standalone/seed-db.ts
+++ src/standalone/seed-db.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../app.module';
-
+import { randEmail } from '@ngneat/falso';
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   // Todo :  Do What you want
-  console.log("SEED DB");
+  console.log("SEED DB" + randEmail());
   
   await app.close();
 }
